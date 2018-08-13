543a47953399325848c06ceb3a6d795002fa93cd
Giu <39471667+gmarinella9@users.noreply.github.com>
2018-08-13 03:06:56 -0300

    Update getComponent.py
    Simplifying Logic

--- getComponent.py
+++ getComponent.py
@@ -1,6 +1,6 @@
 string = 'Objeto.getNombreFuncion("123").getAttribute("Nombre_Etiqueta") && Objeto.getNombreFuncion("Otra_Etiqueta").getAttribute("Nombre_Etiqueta")'
-datos = string.split('"')
+datos = string.split('"')[1::2]
 resultado = []
 
-for i in range(1,len(datos),2):
-	print(datos[i])
+for i in datos: #range(1,len(datos),2):
+	print(i)#print(datos[i])
